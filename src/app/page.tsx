21d29aeeed698f377b05eb9262baa37e292b971d
Florian Pürschel <hello@prshl.de>
2025-06-01 19:42:11 +0200

    Minor config changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Main } from "@/components/main";
 import { Race } from "@/components/race";
 import React from "react";
 
@@ -5,17 +6,7 @@ export default function Home() {
   /** Render */
   return (
     <React.Fragment>
-      <div className="flex flex-col justify-center items-center w-screen h-screen">
-        <h1 data-speed={1} className="font-bold text-6xl text-center">
-          THIS IS A TEST 1
-        </h1>
-        <h1 data-speed={0.95} className="font-bold text-6xl text-center">
-          THIS IS A TEST 1
-        </h1>
-        <h1 data-speed={0.9} className="font-bold text-6xl text-center">
-          THIS IS A TEST 1
-        </h1>
-      </div>
+      <Main />
       <Race index={0} distance={10_000} />
       <Race index={1} distance={42_195} />
     </React.Fragment>
---
+++ src/components/main/main.tsx
@@ -0,0 +1,9 @@
+export function Main() {
+  return (
+    <div className="flex flex-col justify-center items-center w-screen h-screen">
+      <h1 data-speed={1} className="font-bold text-6xl text-center">
+        THIS IS A TEST 1
+      </h1>
+    </div>
+  )
+}
